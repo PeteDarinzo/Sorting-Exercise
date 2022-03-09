e718c97ef97f393721e02ecea089a56c905cbb5f
Peter Darinzo <birel44@yahoo.com>
2022-03-09 17:58:21 -0600

    complete insertion and selection sort

--- selection.js
+++ selection.js
@@ -1,3 +1,21 @@
-function selectionSort() {}
+function selectionSort(arr) {
+  let idx; // helper idx
+  for (let i = 0; i < arr.length; i++) {
+    let min = arr[i];
+    idx = i; // min idx is i
+    for (let j = i; j < arr.length; j++) {
+      if (arr[j] < min) {
+        min = arr[j]; // set new min
+        idx = j;
+      }
+    }
+    if (idx !== i) { // if idx is not i, swap
+      let temp = arr[idx];
+      arr[idx] = arr[i];
+      arr[i] = temp;
+    }
+  }
+  return arr;
+}
 
 module.exports = selectionSort;
